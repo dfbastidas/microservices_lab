c9d984c9c72f5b20b2e981ae52ebcbdef13c28f5
Diego Bastidas <diegofernando150@gmail.com>
2025-08-28 22:39:26 -0500

    added local storage to the project

--- frontend/src/hooks/useVideo.ts
+++ frontend/src/hooks/useVideo.ts
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
-import { videoItems } from "../data/db";
 import type { VideoItem } from "../types";
 
 export default function useVideo() {
-  const[videos, setVideos] = useState(videoItems);
+  // LocalStorage recibi un string como parametro.
+  // Como el valor inicial puede ser nulo, entonces asignamos un valos por defecto
+  // En este caso es []:
+  const storedVideos = JSON.parse(localStorage.getItem('videos') ||"[]") 
+  const[videos, setVideos] = useState<VideoItem[]>(storedVideos);
   
   // El useEffect sirve para: cuando X cambie, haz Y
+  // En este caso nos interesa guardar el video en el local storage.
   useEffect(() => {
+    localStorage.setItem('videos', JSON.stringify(videos))
     console.log(videos)
   }, [videos])
 
